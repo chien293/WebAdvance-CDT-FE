5f48fa1ac2d6a4a4d557daf268bed531c95bea53
Pal Nguyen <pnpal2002@gmail.com>
2023-11-07 17:50:49 +0700

    feat: hero section (landing page)

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css";
 import SignIn from "./auth/SignIn";
 import Nav from "@/components/landing-page/Nav";
-import SwitchTheme from "@/components/landing-page/switch-theme/SwitchTheme";
+import MainContent from "@/components/landing-page/MainContent";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,6 +11,7 @@ export default function Home() {
   return (
     <>
       <Nav></Nav>
+      <MainContent></MainContent>
       {/* <SignIn /> */}
     </>
   );
