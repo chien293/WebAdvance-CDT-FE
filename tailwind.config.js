b958549ee4cd5dc9e603e60b63d892370cf74a18
Pal Nguyen <pnpal2002@gmail.com>
2023-12-06 17:24:50 +0700

    fix: test deloy 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,10 @@ module.exports = {
   darkMode: "class",
   plugins: [
     nextui({
+      prefix: "nextui", // prefix for themes variables
+      addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
+      defaultTheme: "light", // default theme from the themes object
+      defaultExtendTheme: "light", // default theme to extend on custom themes
       themes: {
         light: {
           colors: {
